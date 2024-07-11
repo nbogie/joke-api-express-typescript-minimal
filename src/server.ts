82ececc626fda69e6e2245ca1c44b0f05bc3220c
nbogie <neillbogie@googlemail.com>
2024-07-11 10:26:46 +0100

    wip

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,20 @@
 import express from "express";
 const app = express();
 
-const x: number = "hello";
-console.log("Hello typescript world");
+const x: string = "hello";
+console.log("Hello C9 typescript world");
+
+interface Message {
+    author: string;
+    text: string;
+}
+
+const messages: Message[] = [{ author: "neill", text: "first!" }];
+
+app.get("/messages", (req, res) => {
+    res.json(messages);
+});
+
+app.listen(4000, () => {
+    console.log("server started listening");
+});
